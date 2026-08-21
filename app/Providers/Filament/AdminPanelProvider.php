92adcda86993bed58a41f108b02970067cd526c3
muhamadanasmustopa1112 <muhamadanasmustopa1112@gmail.com>
2026-08-21 09:54:28 +0700

    feat: update admin panel branding to use dynamic logo view

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Emerald,
             ])
             ->brandName('SITOMPUL')
-            ->brandLogo(asset('images/logo.png'))
+            ->brandLogo(fn () => view('filament.admin.logo'))
             ->brandLogoHeight('2.5rem')
             ->favicon(asset('images/logo.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
---
+++ resources/views/filament/admin/logo.blade.php
@@ -0,0 +1,12 @@
+<div class="flex flex-col items-center gap-3 text-center">
+    <img
+        src="{{ asset('images/logo.png') }}"
+        alt="Logo Hatopan Raja Toga Sitompul"
+        class="h-24 w-auto object-contain"
+    >
+
+    <span class="max-w-xs text-sm font-semibold leading-5 text-gray-950 dark:text-white">
+        Hatopan Raja Toga Sitompul<br>
+        Boru, Bere, Ibebere Bandung Raya dan Sekitarnya
+    </span>
+</div>
